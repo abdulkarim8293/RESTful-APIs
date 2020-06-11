23268c152b972988328ae2ba5f3cc662b83d1f0c
unknown <abdulkarim8293@gmail.com>
2020-06-11 21:39:04 +0600

    get all user info

--- app/src/main/java/com/abdulkarim/restfulapis/MainActivity.java
+++ app/src/main/java/com/abdulkarim/restfulapis/MainActivity.java
@@ -45,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
         webServices = retrofit.create(WebServices.class);
 
 
-        //getAllUsersInfo();
+        getAllUsersInfo();
         //getAllPosts();
         //getAllPostComments();
-        getAllAlbums();
+        //getAllAlbums();
 
     }
 
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<Albums>> call, Response<List<Albums>> response) {
 
                 if (response.isSuccessful()){
-                    //Toast.makeText(MainActivity.this, ""+response.body().get(0).getTitle(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, ""+response.body().get(0).getTitle(), Toast.LENGTH_SHORT).show();
                 }
             }
 
